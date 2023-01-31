56bdfd56861e0bf2b80f22286e02cf526c25da3e
KhoiNV02 <khoikakaka123@gmail.com>
2023-01-31 21:20:48 +0700

    Base-Web

--- Khôi/script.js
+++ Khôi/script.js
@@ -1,12 +1,27 @@
+// JavaScript Document
+var slideIndex = 1;
+showSlider(slideIndex);
 
-const inputSearch=document.querySelector(".input-search")
-inputSearch.onkeyup = (e)=>
-{
-	alert("hâha")
+function plusSlider(n) {
+  showSlider(slideIndex += n);
 }
-let relist= [
-	"Paris",
-	"Đà Nẵng",
-	"London",
-	"Rome"
-]
+
+function currentSlider(n) {
+  showSlider(slideIndex = n);
+}
+
+function showSlider(n) {
+  var i;
+  var slider = document.getElementsByClassName("mySlider");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slider.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slider.length}
+  for (i = 0; i < slider.length; i++) {
+      slider[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slider[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
