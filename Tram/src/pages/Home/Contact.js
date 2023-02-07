ec9f7f362183bef2bc1c0174ebf99cec3b63c1f4
Bich Tram <bichtramlt1911@gmail.com>
2023-02-07 21:45:48 +0700

    contact-us

--- Tram/src/pages/Home/Contact.js
+++ Tram/src/pages/Home/Contact.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import contactImg from "../../assets/Contact/contact-us.png";
 
 const Contact = () => {
   return (
     <div className="w-11/12 lg:w-5/6 m-auto py-[40px]">
-      <img alt="contac-us" src={contactImg} />
+      <div className="relative flex items-center justify-center h-[300px]">
+        <img
+          alt="contac-us"
+          src={contactImg}
+          className="absolute top-0 right-0 left-0 h-full w-full object-cover rounded-3xl"
+        />
+        <div className="flex items-center justify-center w-full h-full z-10 bg-[#ff5e1461] rounded-3xl">
+          <Link to="/contact">
+            <button className="button bg-[#ff5e14] text-white z-30">
+              Contac us
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
